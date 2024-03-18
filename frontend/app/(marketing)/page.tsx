80dd99f43d879cb2be961af29226cc893c85dced
Dung Dong <dongtandung2001@gmail.com>
2024-03-18 00:42:16 -0700

    commit

---
+++ frontend/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+      Marketing Page
+    </div>
+  );
+};
+
+export default MarketingPage;
